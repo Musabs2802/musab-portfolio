75e1635b02aed59ea114aa7e582d35518e1ca9e6
Musab Shaikh <musabs2802@gmail.com>
2025-07-25 18:48:08 +0400

    feat: favicon

--- src/data/testimonial.data.ts
+++ src/data/testimonial.data.ts
@@ -1,19 +1,19 @@
 const testimonialData = [
   {
     name: 'Al Kabeer Group',
-    role: '',
+    role: 'Dubai, UAE',
     message:
       "In just four months, Musab has streamlined and automated our accounting process, resulting in significant efficiency improvements",
   },
   {
-    name: 'Naveed Iqbal',
-    role: 'CEO at Tritone Health',
+    name: 'Naveed Iqbal, CEO',
+    role: 'Tritone Health | London, UK',
     message:
       "Musab was the best app developer we ever had",
   },
   {
-    name: 'MBVS Switzerland',
-    role: '',
+    name: 'MBVS',
+    role: 'Zürich, Switzerland',
     message:
       'Musab handled very well the requirements of our app including timely delivery with best quality work and availability',
   },
